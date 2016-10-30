aba91aff581f4e74ffdedbfa79d37ec87286add1
KyleT082 <kyletruebody@gmail.com>
2016-10-30 01:05:04 +0000

    Initial commit

---
+++ app/src/main/java/com/dev/kgt/regstudent/MainActivity.java
@@ -0,0 +1,17 @@
+package com.dev.kgt.regstudent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    //take a picture
+    public void capturePic(){
+
+    }
+}

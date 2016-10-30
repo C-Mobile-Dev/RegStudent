91ed3f9192fff0bfd55e991144f1cae13c035d2b
KyleT082 <kyletruebody@gmail.com>
2016-10-30 01:22:47 +0000

    Added camera

--- app/src/main/java/com/dev/kgt/regstudent/MainActivity.java
+++ app/src/main/java/com/dev/kgt/regstudent/MainActivity.java
@@ -1,7 +1,9 @@
 package com.dev.kgt.regstudent;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,8 +12,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    //take a picture
-    public void capturePic(){
 
+    //take a picture
+    public void capturePic(View view){
+        Intent camInt = new Intent("android.media.action.IMAGE_CAPTURE");
+        startActivityForResult(camInt,0);
     }
 }

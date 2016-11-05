a64a33e960182a6d051df4e8b118e2bd5f690fd9
KyleT082 <kyletruebody@gmail.com>
2016-11-05 17:07:35 +0000

    Much to fix

--- app/src/main/java/com/dev/kgt/regstudent/MainActivity.java
+++ app/src/main/java/com/dev/kgt/regstudent/MainActivity.java
@@ -1,13 +1,17 @@
 package com.dev.kgt.regstudent;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,11 +23,50 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        restoreProfile(null);
+
         Button submitBtn = (Button)findViewById(R.id.submitBtn);
         submitBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 passData(v);
+            }
+        });
+
+        //Get the EditText Obj and validate the contents have been entered
+        final EditText name_Text = (EditText)findViewById(R.id.nameText);
+        name_Text.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus && name_Text.getText().toString().equals("Name")){
+                    name_Text.setText("");
+                }else if(!hasFocus && name_Text.getText().toString().equals("")){
+                        name_Text.setText("Name");
+                }
+            }
+        });
+        //Get the EditText Obj and validate the contents have been entered
+        final EditText studNum_Text = (EditText)findViewById(R.id.stdNumText);
+        studNum_Text.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus && studNum_Text.getText().toString().equals("Student Number")){
+                    studNum_Text.setText("");
+                }else if(!hasFocus && studNum_Text.getText().toString().equals("")){
+                    studNum_Text.setText("Student Number");
+                }
+            }
+        });
+        //Get the EditText Obj and validate the contents have been entered
+        final EditText course_Text = (EditText)findViewById(R.id.courseText);
+        course_Text.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus && course_Text.getText().toString().equals("Course")){
+                    course_Text.setText("");
+                }else if(!hasFocus && course_Text.getText().toString().equals("")){
+                    course_Text.setText("Course");
+                }
             }
         });
     }
@@ -57,9 +100,9 @@ public class MainActivity extends AppCompatActivity {
         EditText edCourse = (EditText)findViewById(R.id.courseText);
         String strCourse = edCourse.getText().toString();
 
-        //Validate fields is not empty
+        //Validate fields are not empty before data submission
         if(strName.isEmpty() || strName.equals("Name") || strStdNum.isEmpty() || strStdNum.equals("Student Number")
-                || strCourse.isEmpty() || strCourse.equals("Course") || bmp.equals(null)){
+                || strCourse.isEmpty() || strCourse.equals("Course") || bmp == null){
 
             Toast.makeText(this,"Some Fields are missing!",Toast.LENGTH_LONG).show();
         }else{
@@ -75,5 +118,16 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public void restoreProfile(View view){
 
+        SharedPreferences settings = getPreferences(0);
+
+        EditText name = (EditText)findViewById(R.id.nameText);
+        name.setText(settings.getString("name","Name"), TextView.BufferType.EDITABLE);
+        EditText stdNum = (EditText)findViewById(R.id.stdNumText);
+        stdNum.setText(settings.getString("stdNum","Student Number"), TextView.BufferType.EDITABLE);
+        EditText course = (EditText)findViewById(R.id.courseText);
+        course.setText(settings.getString("course","Course"), TextView.BufferType.EDITABLE);
+
+    }
 }
--- app/src/main/java/com/dev/kgt/regstudent/Summary.java
+++ app/src/main/java/com/dev/kgt/regstudent/Summary.java
@@ -1,26 +1,33 @@
 package com.dev.kgt.regstudent;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * Created by Kyle on 30/10/2016.
  *
- * Prompts the user to accept the photo taken
- * or retry.
  */
 
 public class Summary extends AppCompatActivity{
 
     private Bitmap bmp;
     private String name,stdNum,course;
+    private String fileName = "bmp.PNG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,10 +49,10 @@ public class Summary extends AppCompatActivity{
         edName.setText("Name : \t\t\t\t\t" + name);
 
         TextView edStdNum = (TextView)findViewById(R.id.stdNumText);
-        edStdNum.setText("Student Number : " + stdNum);
+        edStdNum.setText("Student Number:\t" + stdNum);
 
         TextView edCourse = (TextView)findViewById(R.id.courseText);
-        edCourse.setText("Course : \t\t\t\t\t " + course);
+        edCourse.setText("Course :\t\t\t\t\t" + course);
 
         //back button function
         Button backBtn = (Button)findViewById(R.id.back);
@@ -56,11 +63,44 @@ public class Summary extends AppCompatActivity{
                 startActivityForResult(intent,0);
             }
         });
+
+
+
     }
 
+    @Override
+    public void onStop(){
+        super.onStop();
+        saveProfile(null);
+    }
 
+    //Saves all the users profile data to shared-prefs and saves the image to file
+    public void saveProfile(View view){
 
+        SharedPreferences profile = getPreferences(0);
+        SharedPreferences.Editor editor = profile.edit();
 
+        editor.putString("name",name);
+        editor.putString("StdNum",stdNum);
+        editor.putString("course",course);
+        System.out.print( "Got this far");
 
+        //NOT WORKING :(
+        try{
 
+            File file = Environment.getExternalStorageDirectory();
+            File dest = new File(file,fileName);
+            FileOutputStream fout = new FileOutputStream(dest);
+            bmp.compress(Bitmap.CompressFormat.PNG,100,fout);
+            fout.flush();
+            fout.close();
+
+            Toast.makeText(this,"Profile settings have been saved!",Toast.LENGTH_SHORT).show();
+        }catch(IOException e){
+            e.printStackTrace();
+            Toast.makeText(this,"Error Saving the Picture",Toast.LENGTH_LONG).show();
+        }
+
+        editor.apply();
+    }
 }
